88a0e0fd4017c2c29e56cd8f37fc856548d47b98
Denis Oliver Kropp <Leichenbegatter@outlook.com>
2021-01-06 12:24:03 +0100

    test_cl_objects_simple/queue fixes, updates

--- test_cl/test_cl.cpp
+++ test_cl/test_cl.cpp
@@ -10,23 +10,43 @@
 
 static void test_cl_queue();
 static void test_cl_list();
-//static void test_cl_objects();
+static void test_cl_objects_simple();
+static void test_cl_objects_queue();
 
 
 int main()
 {
 	parallel_f::setDebugLevel(0);
-	parallel_f::system::instance().setAutoFlush(parallel_f::system::AutoFlush::EndOfLine);
+//	parallel_f::setDebugLevel("task::", 1);
+//	parallel_f::setDebugLevel("task_list::", 1);
+//	parallel_f::setDebugLevel("task_queue::", 1);
+//	parallel_f::setDebugLevel("task_node::", 1);
+	parallel_f::setDebugLevel("task_cl::cl_task::", 1);
+//	parallel_f::setDebugLevel("task_cl::kernel_pre::", 1);
+	parallel_f::setDebugLevel("task_cl::kernel_exec::", 1);
+//	parallel_f::setDebugLevel("task_cl::kernel_post::", 1);
+
+//	parallel_f::system::instance().setAutoFlush(parallel_f::system::AutoFlush::EndOfLine);
+//	parallel_f::system::instance().startFlushThread(10);
 
 
-	test_cl_queue();
+	for (int i = 0; i < 10; i++) {
+		test_cl_queue();
+		parallel_f::stats::instance::get().show_stats();
+		parallel_f::system::instance().flush();
+	}
+
+	test_cl_list();
 	parallel_f::stats::instance::get().show_stats();
+	parallel_f::system::instance().flush();
 
-//	test_cl_list();
-//	parallel_f::stats::instance::get().show_stats();
+	test_cl_objects_simple();
+	parallel_f::stats::instance::get().show_stats();
+	parallel_f::system::instance().flush();
 
-	//	test_cl_objects();
-	//	parallel_f::stats::instance::get().show_stats();
+	test_cl_objects_queue();
+	parallel_f::stats::instance::get().show_stats();
+	parallel_f::system::instance().flush();
 }
 
 
@@ -36,12 +56,12 @@ static void test_cl_queue()
 {
 	std::vector<std::byte> data(16 * 1024 * 1024);
 
-	task_cl::task_cl_kernel_args args(new task_cl::task_cl_kernel_args::kernel_arg_mem(data.size(), data.data(), NULL),
-									  new task_cl::task_cl_kernel_args::kernel_arg_mem(data.size(), NULL, data.data()),
-									  new task_cl::task_cl_kernel_args::kernel_arg_t<cl_int>((cl_int)data.size() / 16));
+	auto args = std::make_shared<task_cl::kernel_args>(new task_cl::kernel_args::kernel_arg_mem(data.size(), data.data(), NULL),
+															   new task_cl::kernel_args::kernel_arg_mem(data.size(), NULL, data.data()),
+															   new task_cl::kernel_args::kernel_arg_t<cl_int>((cl_int)data.size() / 16));
 
-	auto task_pre = task_cl::task_cl_kernel_pre::make_task(args);
-	auto task_post = task_cl::task_cl_kernel_post::make_task(args);
+	auto task_pre = task_cl::kernel_pre::make_task(args);
+	auto task_post = task_cl::kernel_post::make_task(args);
 
 	parallel_f::task_queue tq;
 	parallel_f::joinables j;
@@ -51,8 +71,8 @@ static void test_cl_queue()
 
 	auto kernel = task_cl::make_kernel("cltest.cl", "CLTest2", data.size() / 16, 256);
 
-	for (int i = 0; i < 3; i++) {
-		auto task_exec = task_cl::task_cl_kernel_exec::make_task(args, kernel);
+	for (int i = 0; i < 4; i++) {
+		auto task_exec = task_cl::kernel_exec::make_task(args, kernel);
 
 		tq.push(task_exec);
 
@@ -72,23 +92,29 @@ static void test_cl_list()
 {
 	std::vector<std::byte> src(16 * 1024 * 1024), dst(src.size());
 
-	task_cl::task_cl_kernel_args kernel_args(new task_cl::task_cl_kernel_args::kernel_arg_mem(src.size(), src.data(), NULL),
-											 new task_cl::task_cl_kernel_args::kernel_arg_mem(dst.size(), NULL, dst.data()),
-											 new task_cl::task_cl_kernel_args::kernel_arg_t<cl_int>((cl_int)src.size() / 16));
-
 	parallel_f::task_list tl;
 
+	auto kernel = task_cl::make_kernel("cltest.cl", "CLTest2", src.size() / 16, 256);
+
 	for (int i = 0; i < 3; i++) {
-		auto task = task_cl::make_task("cltest.cl", "CLTest2", src.size() / 16, 256, kernel_args);
+		auto args = std::make_shared<task_cl::kernel_args>(new task_cl::kernel_args::kernel_arg_mem(src.size(), src.data(), NULL),
+														   new task_cl::kernel_args::kernel_arg_mem(dst.size(), NULL, dst.data()),
+														   new task_cl::kernel_args::kernel_arg_t<cl_int>((cl_int)src.size() / 16));
+
+		auto task = task_cl::make_task(kernel, args);
 
 		tl.append(task);
+
+		tl.flush();
 	}
 
 	tl.finish();
 }
 
-/*
-static void test_cl_objects()
+
+// parallel_f :: task_cl == testing OpenCL kernel execution (with objects)
+
+static void test_cl_objects_simple()
 {
 	class object
 	{
@@ -96,6 +122,14 @@ static void test_cl_objects()
 		int x;
 		int y;
 		int seq;
+
+		object()
+			:
+			x(0),
+			y(0),
+			seq(0)
+		{
+		}
 	};
 
 	std::vector<object> objects(1000);
@@ -103,30 +137,82 @@ static void test_cl_objects()
 	parallel_f::task_list tl;
 	parallel_f::task_id flush_id = 0;
 
-	task_cl::task_cl_kernel_args kernel_args(new task_cl::task_cl_kernel_args::kernel_arg_mem(objects.size() * sizeof(object), objects.data(), objects.data()),
-											 new task_cl::task_cl_kernel_args::kernel_arg_t<cl_int>((cl_int)objects.size()));
+	auto kernel = task_cl::make_kernel("cltest.cl", "RunObjects", objects.size(), 100);
 
 	for (int i = 0; i < 20; i++) {
-		auto task = task_cl::make_task("cltest.cl", "RunObjects", objects.size(), 100, kernel_args);
+		auto args = std::make_shared<task_cl::kernel_args>(new task_cl::kernel_args::kernel_arg_mem(objects.size() * sizeof(object), objects.data(), objects.data()),
+																   new task_cl::kernel_args::kernel_arg_t<cl_int>((cl_int)objects.size()));
 
-		auto run_id = tl.append(task, flush_id);
+		auto task = task_cl::make_task(kernel, args);
 
-		auto log_task = parallel_f::make_task([&objects]() {
-			parallel_f::logInfo("log_task::run()...\n");
+		auto task_id = tl.append(task, flush_id);
 
+
+		auto task_log = parallel_f::make_task([&objects]() {
 			parallel_f::logInfo("object seq %d\n", objects[0].seq);
-
-			parallel_f::logInfo("log_task::run() done.\n");
-
-			return parallel_f::none;
 		});
 
-		tl.append(log_task, run_id);
+		tl.append(task_log, task_id);
 
-//		flush_id = tl.flush();
-		tl.finish();
+		
+		flush_id = tl.flush();
 	}
 
-//	tl.finish();
+	tl.finish();
 }
-*/
+
+
+// parallel_f :: task_cl == testing OpenCL kernel execution (with objects)
+
+static void test_cl_objects_queue()
+{
+	class object
+	{
+	public:
+		int x;
+		int y;
+		int seq;
+
+		object()
+			:
+			x(0),
+			y(0),
+			seq(0)
+		{
+		}
+	};
+
+	std::vector<object> objects(1000);
+
+	parallel_f::task_queue tq;
+	parallel_f::joinable j;
+
+	auto kernel = task_cl::make_kernel("cltest.cl", "RunObjects", objects.size(), 100);
+
+	for (int i = 0; i < 20; i++) {
+		auto args = std::make_shared<task_cl::kernel_args>(new task_cl::kernel_args::kernel_arg_mem(objects.size() * sizeof(object), objects.data(), objects.data()),
+																   new task_cl::kernel_args::kernel_arg_t<cl_int>((cl_int)objects.size()));
+
+		auto task_pre = task_cl::kernel_pre::make_task(args);
+		auto task_exec = task_cl::kernel_exec::make_task(args, kernel);
+		auto task_post = task_cl::kernel_post::make_task(args);
+
+		tq.push(task_pre);
+		tq.push(task_exec);
+		tq.push(task_post);
+
+
+		auto task_log = parallel_f::make_task([&objects]() {
+			parallel_f::logInfo("object seq %d\n", objects[0].seq);
+		});
+
+		tq.push(task_log);
+
+
+		j.join();
+
+		j = tq.exec(true);
+	}
+
+	j.join();
+}
