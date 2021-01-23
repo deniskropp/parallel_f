6a8bcea7f837960adc3b5a256e0b1b3921059ca1
Denis Oliver Kropp <Leichenbegatter@outlook.com>
2021-01-23 10:59:32 +0100

    add small test

--- parallel_f.cpp
+++ parallel_f.cpp
@@ -3,3 +3,19 @@
 
 #include "parallel_f.hpp"
 
+int main()
+{
+	parallel_f::system::instance().setAutoFlush(parallel_f::system::AutoFlush::EndOfLine);
+
+	for (int n = 0; n < 100; n++) {
+		parallel_f::task_queue tq;
+
+		for (int i = 0; i < 100; i++)
+			tq.push(parallel_f::make_task([]() {}));
+
+		tq.exec();
+	}
+
+	parallel_f::stats::instance().show_stats();
+	parallel_f::system::instance().flush();
+}
